4a8d256effc32b5e5759230c3acae24c5b0d9e6c
Amyrald <geraldimaezue@gmail.com>
2021-03-07 14:11:28 -0500

    Step 2 of assignment

--- split.py
+++ split.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-"""One-line description of the program goes here."""
+"""This programs takes in an input file containing text data then splits it and outputs a train set (80%), a develpoment set (10%) and a test set (10%)."""
+
 from typing import Iterator, List
 import argparse
+import random
 
+#Generator function takes in a file path and creates a text data in nested lists
 def read_tags(path: str) -> Iterator[List[List[str]]]:
     with open(path, "r") as source:
         lines = []
@@ -17,22 +20,24 @@ def read_tags(path: str) -> Iterator[List[List[str]]]:
     if lines:
         yield lines
 
+#Writes text data in nested lists into a new file
 def write_tag(data_set: list, file_tag: str) -> Iterator[List[List[str]]]:
     with open(file_tag, 'w') as filehandle:
         for row in data_set:
-            for word in row:
-                print(word, file = filehandle)       
-
+            for sent in row:
+                print(sent, file = filehandle)   
+      
+#Command line argument parsers
 def main(args: argparse.Namespace) -> None:
     corpus = list(read_tags(args.input))
+    random.shuffle(corpus)
     train_set = corpus[0: int(len(corpus)*0.8)-1]
     dev_set = corpus[int(len(corpus)*0.8) : int(-len(corpus)*0.1)]
     test_set = corpus[int(-len(corpus)*0.1): ]
     write_tag(train_set, args.train)
     write_tag(dev_set, args.dev)
     write_tag(test_set, args.test)
-     # TODO: do the work here.
-    ...
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -41,7 +46,3 @@ if __name__ == "__main__":
     parser.add_argument("dev")
     parser.add_argument("test")
     main(parser.parse_args())
-
-    # TODO: declare arguments.
-    # TODO: parse arguments and pass them to `main`.
-    ...
